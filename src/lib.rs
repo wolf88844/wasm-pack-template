0e9cb569344b2f8b073048cd96bdfafe19ca744e
wolf88844 <wolf88840@163.com>
2025-02-26 15:59:02 +0800

    feat(greet): 更新 greet 函数以接受名称参数
    - 修改 greet 函数，使其接受一个名称参数
    - 使用格式化字符串动态生成问候语
    - 移除了 init 和 run 函数

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,11 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn init(){
-    console_error_panic_hook::set_once();
-}
-
-#[wasm_bindgen(start)]
-pub fn run(){
-    greet();
-}
-
-#[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, {{project-name}}!");
+pub fn greet(name:&str) {
+    alert(&format!("Hello, {}!", name));
 }
